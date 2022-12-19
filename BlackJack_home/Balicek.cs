939a26cb83d99f40007ca0be95dc811dd1eb4c23
Denis Žižka <zizkade20@sps-prosek.cz>
2022-12-19 13:41:26 +0100

    doprdele achjo

--- BlackJack_home/Balicek.cs
+++ BlackJack_home/Balicek.cs
@@ -51,7 +51,7 @@ namespace BlackJack
             List<string> DealerCards = new List<string>();
             Random ran = new Random();
 
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 1; i++)
             {
                 int num = ran.Next(DeckOfCards.Count);
 
@@ -61,7 +61,7 @@ namespace BlackJack
             }
             
 
-            /*
+            
             int indexh1 = random.Next(DeckOfCards.Count);
             int indexh2 = random.Next(DeckOfCards.Count);
 
@@ -72,7 +72,7 @@ namespace BlackJack
             PlayerCards.Add(DeckOfCards[indexh1]);
             
             DealerCards.Add(DeckOfCards[indexd1]);
-            */
+            
             Hrac Hrac1 = new Hrac();
 
             Console.WriteLine("Zadej sazku:");
@@ -264,6 +264,15 @@ namespace BlackJack
                             {
                                 //DealerCards.Add(DeckOfCards[indexd2++]);
 
+                                for (int i = 0; i < 1; i++)
+                                {
+                                    int num = ran.Next(DeckOfCards.Count);
+
+                                    DealerCards.Add(DeckOfCards[num]);
+                                    DeckOfCards.RemoveAt(num);
+
+                                }
+
                                 Console.Write("Vase Karty: ");
                                 foreach (var j in PlayerCards)
                                 {
@@ -306,7 +315,8 @@ namespace BlackJack
 
             
         }
-       /* internal int CountCards(List<Karta> Karty)
+        /*
+        internal int CountCards(List<Karta> Karty)
         {
             int summH = 0;
 
@@ -363,18 +373,254 @@ namespace BlackJack
                 }
             }
             return summH;
-        }*/
+        }
+        */
   
     }
 
 
 }
 
+/* 
+namespace BlackJack
+{
+    internal class Balicek
+    {
+        internal List<Karta> Deck;
+        private string[] strings1;
+        private string[] strings2;
+
+        public Balicek(string[] strings1, string[] strings2)
+        {
+            this.strings1 = strings1;
+            this.strings2 = strings2;
+        }
+
+        internal void CreateDeck(string[] symboly, string[] hodnoty)
+        {
+            List<Karta> Deck = new List<Karta>();
+
+
+            foreach (string symbol in symboly)
+            {
+                foreach (string hodnota in hodnoty)
+                {
+                    Deck.Add(new Karta(hodnota, symbol));
+
+                }
+
+            }
+
+        }
+        internal List<Karta> Lizni(int Pocet) {
+
+            List<string> DeckOfCards = new List<string>();
+
+            foreach (var i in Deck)
+            {
+                DeckOfCards.Add(i.VratNazevKarty());
+            }
+
+
+            List<string> PlayerCards = new List<string>();
+            List<string> DealerCards = new List<string>();
+
+            Random ran = new Random();
+
+            for (int i = 0; i < 2; i++)
+            {
+                int num = ran.Next(DeckOfCards.Count);
+
+                PlayerCards.Add(DeckOfCards[num]);
+                DeckOfCards.RemoveAt(num);
+
+            }
+            return PlayerCards;
+        }
+
+            
+        internal void Zkontrolovat() { 
+            Hrac Hrac1 = new Hrac();
+
+            Console.WriteLine("Zadej sazku:");
+            int sazka = Int32.Parse(Console.ReadLine());
+            Hrac1.Penize -= sazka;
+
+            int summH = 0;
+            int summD = 0;
+            bool hit = true;
+            while (hit)
+            {
+
+
+                // AHOJ
+                Console.WriteLine("Zůstatek: " + Hrac1.Penize + "$\n");
+
+
+                Console.Write("Vase Karty: ");
+                foreach (var j in PlayerCards)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+                Console.Write("Krupierovy Karty: ");
+                foreach (var k in DealerCards)
+                {
+                    Console.Write(k + " ");
+                }
+                Console.WriteLine();
 
 
 
+                Console.WriteLine("\nSoučet vašich karet je: " + summH);
+                Console.WriteLine("Součet dealerových karet je: " + summD);
 
-          
+
+
+                Console.WriteLine("\nChcete líznout další kartu? (Y/N)");
+                string volba = Console.ReadLine().ToLower();
+                switch (volba)
+                {
+                    case "y":
+                        //PlayerCards.Add(DeckOfCards[indexh2]);
+                        for (int i = 0; i < 1; i++)
+                        {
+                            int num = ran.Next(DeckOfCards.Count);
+
+                            PlayerCards.Add(DeckOfCards[num]);
+                            DeckOfCards.RemoveAt(num);
+
+                        }
+                        if (summH > 21)
+                        {
+                            hit = false;
+                            Console.WriteLine("Máš přes 21, prohál jsi! :(");
+                        }
+
+                        break;
+                    case "n":
+                        hit = false;
+
+                        if (summH == 21)
+                        {
+                            Console.WriteLine("Máš blackjack gratuluji! :)");
+                            Hrac1.Penize += 2 * sazka;
+                        }
+
+                        else
+                        {
+                            while (summD < summH && summH! > 21)
+                            {
+                                //DealerCards.Add(DeckOfCards[indexd2++]);
+
+                                Console.Write("Vase Karty: ");
+                                foreach (var j in PlayerCards)
+                                {
+                                    Console.Write(j + " ");
+                                }
+                                Console.WriteLine();
+                                Console.Write("Krupierovy Karty: ");
+                                foreach (var k in DealerCards)
+                                {
+                                    Console.Write(k + " ");
+                                }
+                                Console.WriteLine();
+
+                                Console.WriteLine("\nSoučet vašich karet je: " + summH);
+                                Console.WriteLine("Součet dealerových karet je: " + summD);
+
+                                if (summD > 21)
+                                {
+                                    Console.WriteLine("Dealer má přes 21, vyhrál jsi! :) ");
+                                    Hrac1.Penize += 2 * sazka;
+                                }
+                                else if (summD > summH && summD! > 21)
+                                {
+                                    Console.WriteLine("Dealer má více, prohrál jsi! :(");
+                                }
+
+                            }
+                        }
+
+
+
+                        Console.WriteLine("Stiskni libovolnou klávesu");
+                        Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("Vyber z nabídky!");
+                        break;
+                }
+            }
+
+
+        }
+         internal int CountCards(List<Karta> Karty)
+         {
+             int summH = 0;
+
+             foreach (var j in Karty)
+             {
+                 switch (j.Hodnota)
+                 {
+                     case "A":
+                         if (summH > 11)
+                         {
+                             summH = +1;
+                         }
+                         else
+                         {
+                             summH = +11;
+                         }
+                         break;
+                     case "2":
+                         summH = +2;
+                         break;
+                     case "3":
+                         summH = +3;
+                         break;
+                     case "4":
+                         summH = +4;
+                         break;
+                     case "5":
+                         summH = +5;
+                         break;
+                     case "6":
+                         summH = +6;
+                         break;
+                     case "7":
+                         summH = +7;
+                         break;
+                     case "8":
+                         summH = +8;
+                         break;
+                     case "9":
+                         summH = +9;
+                         break;
+                     case "10":
+                         summH = +10;
+                         break;
+                     case "J":
+                         summH = +11;
+                         break;
+                     case "Q":
+                         summH = +11;
+                         break;
+                     case "K":
+                         summH = +11;
+                         break;
+                 }
+             }
+             return summH;
+         }
+
+    }
+
+
+}
+*/
+
+
 
 
 
--- BlackJack_home/Hra.cs
+++ BlackJack_home/Hra.cs
@@ -57,3 +57,59 @@ namespace BlackJack
         }
     }
 }
+
+/*
+namespace BlackJack
+{
+    internal class Hra
+    {
+        internal bool Game;
+        internal Balicek Balicek { get; set; }
+
+        internal Hra()
+        {
+            Game = true;
+        }
+
+        public void Loop()
+        {
+            Hrac hrac1 = new Hrac();
+            hrac1.SetName();
+
+
+
+            while (Game)
+            {
+                Console.WriteLine("\n(P)LAY\n(Z)EBRICEK\n(J)AK HRÁT?\n(O) HRE\n(E)XIT");
+                Console.Write("->");
+                string menuVolba = Console.ReadLine().ToLower();
+
+                switch (menuVolba)
+                {
+                    case "p":
+
+                        //Balicek1.VratKartu();
+                        var Balicek = new Balicek(new string[] { "♥", "♦", "♣", "♠" }, new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" });
+                        var hracBalicek = Balicek.Lizni(2);
+                        Console.WriteLine(Balicek.CountCards(hracBalicek));
+                        //Balicek1.CountCards();
+
+                        break;
+                    case "z":
+                        break;
+                    case "j":
+                        Console.WriteLine("Cíl hry je se co nejblíže přiblížit součtem karet k 21. Pokud ho překročíš, automaticky prohráváš.");
+                        break;
+                    case "o":
+                        Console.WriteLine("Black Jack (nebo též Blackjack) je jednou z nejoblíbenějších a nejrozšířenějších karetních her, s kterou se můžete v kasinu setkat. Black Jack je vyhledáván hráči, kteří se nespokojí pouze s tím, aby výsledek hry závisel čistě na náhodě. Naopak, mohou a chtějí svou šanci na výhru ovlivnit vhodně zvolenou strategií. Při nejlepším způsobu hry je dokonce možné nad krupiérem získat určitou výhodu, což je u jiných her jev prakticky nevídaný.");
+                        break;
+                    case "e":
+                        Game = false;
+                        break;
+                }
+
+            }
+        }
+    }
+}
+*/
