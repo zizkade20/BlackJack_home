1c36888240f65ce5bc8a759613e584e7fe0499a0
Denis Žižka <zizkade20@sps-prosek.cz>
2022-12-19 14:37:20 +0100

    scitani jakz takz

--- BlackJack_home/Balicek.cs
+++ BlackJack_home/Balicek.cs
@@ -51,7 +51,7 @@ namespace BlackJack
             List<string> DealerCards = new List<string>();
             Random ran = new Random();
 
-            for (int i = 0; i < 1; i++)
+            for (int i = 0; i < 2; i++)
             {
                 int num = ran.Next(DeckOfCards.Count);
 
@@ -59,20 +59,16 @@ namespace BlackJack
                 DeckOfCards.RemoveAt(num);
 
             }
-            
+            for (int i = 0; i < 1; i++)
+            {
+                int num = ran.Next(DeckOfCards.Count);
 
-            
-            int indexh1 = random.Next(DeckOfCards.Count);
-            int indexh2 = random.Next(DeckOfCards.Count);
+                DealerCards.Add(DeckOfCards[num]);
+                DeckOfCards.RemoveAt(num);
 
-            int indexd1 = random.Next(DeckOfCards.Count);
-            int indexd2 = random.Next(DeckOfCards.Count);
-            
+            }
 
-            PlayerCards.Add(DeckOfCards[indexh1]);
-            
-            DealerCards.Add(DeckOfCards[indexd1]);
-            
+
             Hrac Hrac1 = new Hrac();
 
             Console.WriteLine("Zadej sazku:");
@@ -84,6 +80,21 @@ namespace BlackJack
             bool hit = true;
             while (hit)
             {
+                /*
+                if (summH > 21)
+                {
+                    hit = false;
+                    Console.WriteLine("Prohrál jsi, máš přes 21");
+
+                }
+                if (summD > 21)
+                {
+                    hit = false;
+                    Console.WriteLine("Vyhral Jsi, dealer má přes 21");
+                    Hrac1.Penize += sazka;
+                }
+                */
+                
 
                 foreach (var j in PlayerCards)
                 {
@@ -92,66 +103,52 @@ namespace BlackJack
                         case "A♣" or "A♥" or "A♦" or "A♠":
                             if (summH > 11)
                             {
-                                summH = +1;
+                                summH += 1;
                             }
                             else
                             {
-                                summH = +11;
+                                summH += 11;
                             }
                             break;
                         case "2♣" or "2♥" or "2♦" or "2♠":
-                            summH = +2;
+                            summH += 2;
                             break;
                         case "3♣" or "3♥" or "3♦" or "3♠":
-                            summH = +3;
+                            summH += 3;
                             break;
                         case "4♣" or "4♥" or "4♦" or "4♠":
-                            summH = +4;
+                            summH += 4;
                             break;
                         case "5♣" or "5♥" or "5♦" or "5♠":
-                            summH = +5;
+                            summH += 5;
                             break;
                         case "6♣" or "6♥" or "6♦" or "6♠":
-                            summH = +6;
+                            summH += 6;
                             break;
                         case "7♣" or "7♥" or "7♦" or "7♠":
-                            summH = +7;
+                            summH += 7;
                             break;
                         case "8♣" or "8♥" or "8♦" or "8♠":
-                            summH = +8;
+                            summH += 8;
                             break;
                         case "9♣" or "9♥" or "9♦" or "9♠":
-                            summH = +9;
+                            summH += 9;
                             break;
                         case "10♣" or "10♥" or "10♦" or "10♠":
-                            summH = +10;
+                            summH += 10;
                             break;
                         case "J♣" or "J♥" or "J♦" or "J♠":
-                            summH = +11;
+                            summH += 10;
                             break;
                         case "Q♣" or "Q♥" or "Q♦" or "Q♠":
-                            summH = +11;
+                            summH += 10;
                             break;
                         case "K♣" or "K♥" or "K♦" or "K♠":
-                            summH = +11;
+                            summH += 10;
                             break;
                     }
                 }
 
-                
-                if (summH > 21)
-                {
-                    hit = false;
-                    Console.WriteLine("Prohrál jsi, máš přes 21");
-
-                }
-                if (summD > 21) {
-                    hit = false;
-                    Console.WriteLine("Vyhral Jsi, dealer má přes 21");
-                    Hrac1.Penize += sazka;
-                }
-
-                
                 foreach (var k in DealerCards)
                 {
                     switch (k)
@@ -159,54 +156,55 @@ namespace BlackJack
                         case "A♣" or "A♥" or "A♦" or "A♠":
                             if (summD > 11)
                             {
-                                summD =+ 1;
+                                summD += 1;
                             }
-                            else {
-                                summD =+ 11;
+                            else
+                            {
+                                summD += 11;
                             }
                             break;
                         case "2♣" or "2♥" or "2♦" or "2♠":
-                            summD =+ 2;
+                            summD += 2;
                             break;
                         case "3♣" or "3♥" or "3♦" or "3♠":
-                            summD =+3;
+                            summD += 3;
                             break;
                         case "4♣" or "4♥" or "4♦" or "4♠":
-                            summD =+ 4;
+                            summD += 4;
                             break;
                         case "5♣" or "5♥" or "5♦" or "5♠":
-                            summD =+ 5;
+                            summD += 5;
                             break;
                         case "6♣" or "6♥" or "6♦" or "6♠":
-                            summD =+ 6;
+                            summD += 6;
                             break;
                         case "7♣" or "7♥" or "7♦" or "7♠":
-                            summD =+ 7;
+                            summD += 7;
                             break;
                         case "8♣" or "8♥" or "8♦" or "8♠":
-                            summD =+ 8;
+                            summD += 8;
                             break;
                         case "9♣" or "9♥" or "9♦" or "9♠":
-                            summD =+ 9;
+                            summD += 9;
                             break;
                         case "10♣" or "10♥" or "10♦" or "10♠":
-                            summD =+ 10;
+                            summD += 10;
                             break;
                         case "J♣" or "J♥" or "J♦" or "J♠":
-                            summD =+ 11;
+                            summD += 10;
                             break;
                         case "Q♣" or "Q♥" or "Q♦" or "Q♠":
-                            summD =+ 11;
+                            summD += 10;
                             break;
                         case "K♣" or "K♥" or "K♦" or "K♠":
-                            summD =+ 11;
+                            summD += 10;
                             break;
                     }
                 }
-                                
+
                 // AHOJ
                 Console.WriteLine("Zůstatek: " + Hrac1.Penize + "$\n");
-                
+
 
                 Console.Write("Vase Karty: ");
                 foreach (var j in PlayerCards)
@@ -221,49 +219,47 @@ namespace BlackJack
                 }
                 Console.WriteLine();
 
-                
 
                 Console.WriteLine("\nSoučet vašich karet je: " + summH);
                 Console.WriteLine("Součet dealerových karet je: " + summD);
 
-                
-
-                Console.WriteLine("\nChcete líznout další kartu? (Y/N)");
-                string volba = Console.ReadLine().ToLower();
-                switch (volba)
+                if (summH < 21)
                 {
-                    case "y":
-                        //PlayerCards.Add(DeckOfCards[indexh2]);
-                        for (int i = 0; i < 1; i++)
-                        {
-                            int num = ran.Next(DeckOfCards.Count);
 
-                            PlayerCards.Add(DeckOfCards[num]);
-                            DeckOfCards.RemoveAt(num);
-
-                        }
-                        if (summH > 21)
-                        {
-                            hit = false;
-                            Console.WriteLine("Máš přes 21, prohál jsi! :(");
-                        }
-
-                        break;
-                    case "n":
-                        hit = false;
-
-                        if (summH == 21)
-                        {
-                            Console.WriteLine("Máš blackjack gratuluji! :)");
-                            Hrac1.Penize += 2*sazka;
-                        }
-                        
-                        else
-                        {
-                            while (summD < summH && summH! > 21)
+                    Console.WriteLine("\nChcete líznout další kartu? (Y/N)");
+                    string volba = Console.ReadLine().ToLower();
+                    switch (volba)
+                    {
+                        case "y":
+                            //PlayerCards.Add(DeckOfCards[indexh2]);
+                            for (int i = 0; i < 1; i++)
                             {
-                                //DealerCards.Add(DeckOfCards[indexd2++]);
+                                int num = ran.Next(DeckOfCards.Count);
 
+                                PlayerCards.Add(DeckOfCards[num]);
+                                DeckOfCards.RemoveAt(num);
+
+                            }
+                            if (summH > 21)
+                            {
+                                hit = false;
+                                Console.WriteLine("Máš přes 21, prohál jsi! :(");
+                            }
+
+                            break;
+                        case "n":
+                            hit = false;
+
+                            if (summH == 21)
+                            {
+                                Console.WriteLine("Máš blackjack gratuluji! :)");
+                                Hrac1.Penize += 2 * sazka;
+                            }
+
+                            else
+                            {
+                                
+                                /*
                                 for (int i = 0; i < 1; i++)
                                 {
                                     int num = ran.Next(DeckOfCards.Count);
@@ -272,7 +268,7 @@ namespace BlackJack
                                     DeckOfCards.RemoveAt(num);
 
                                 }
-
+                                */
                                 Console.Write("Vase Karty: ");
                                 foreach (var j in PlayerCards)
                                 {
@@ -292,26 +288,32 @@ namespace BlackJack
                                 if (summD > 21)
                                 {
                                     Console.WriteLine("Dealer má přes 21, vyhrál jsi! :) ");
-                                    Hrac1.Penize += 2*sazka;
+                                    Hrac1.Penize += 2 * sazka;
                                 }
                                 else if (summD > summH && summD! > 21)
                                 {
                                     Console.WriteLine("Dealer má více, prohrál jsi! :(");
                                 }
+
                                 
                             }
-                        }
-                        
-                        
 
-                        Console.WriteLine("Stiskni libovolnou klávesu");
-                        Console.ReadLine();
-                        break;
-                    default:
-                        Console.WriteLine("Vyber z nabídky!");
-                        break;
+
+
+                            Console.WriteLine("Stiskni libovolnou klávesu");
+                            Console.ReadLine();
+                            break;
+                        default:
+                            Console.WriteLine("Vyber z nabídky!");
+                            break;
+                    }
+                } else if (summH > 21)
+                {
+                    hit = false;
+                    Console.WriteLine("Máš přes 21, prohál jsi! :(");
                 }
             }
+                
 
             
         }
@@ -381,7 +383,7 @@ namespace BlackJack
 
 }
 
-/* 
+/*
 namespace BlackJack
 {
     internal class Balicek
@@ -412,27 +414,27 @@ namespace BlackJack
             }
 
         }
-        internal List<Karta> Lizni(int Pocet) {
-
+        internal List<Karta> Lizni(int Pocet, List<Karta> Karty) {
+                
             List<string> DeckOfCards = new List<string>();
 
-            foreach (var i in Deck)
+            foreach (var i in Karty)
             {
                 DeckOfCards.Add(i.VratNazevKarty());
             }
+           
 
-
-            List<string> PlayerCards = new List<string>();
-            List<string> DealerCards = new List<string>();
+            List<Karta> PlayerCards = new List<Karta>();
+            //List<string> DealerCards = new List<string>();
 
             Random ran = new Random();
 
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < Pocet; i++)
             {
-                int num = ran.Next(DeckOfCards.Count);
+                int num = ran.Next(Karty.Count);
 
-                PlayerCards.Add(DeckOfCards[num]);
-                DeckOfCards.RemoveAt(num);
+                PlayerCards.Add(Karty[num]);
+                Karty.RemoveAt(num);
 
             }
             return PlayerCards;
