4f3330fb475ff00024ef7419a35c018b150599a6
Denis Žižka <100208817+zizkade20@users.noreply.github.com>
2022-12-20 22:16:12 +0100

    Update Hrac.cs

--- BlackJack_home/Hrac.cs
+++ BlackJack_home/Hrac.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,34 +8,23 @@ namespace BlackJack
 {
     public class Hrac
     {
-        public string Jmeno { get; set; }
-        //public List<Karta> Ruka { get; set; }
-        public int Skore = 0;
+        public string Jmeno;
         public int Penize = 200;
-        public int sazka;
+        public int Score = 0;
+        public bool Win;
+        public bool Blackjack;
+
+
 
         internal Hrac() { }
         internal Hrac(string _jmeno)
         {
             this.Jmeno = _jmeno;
         }
+        
+        
 
-        internal void SetName()
-        {
-            Console.WriteLine("Zadejte přezdívku:");
-            Console.Write("->");
-            string username = Console.ReadLine();
-            Console.WriteLine("\nVítej " + username);
-            Hrac Hrac1 = new Hrac();
-            Hrac1.Jmeno = username;
-
-            
-        }
-        /*internal void DisplayName()
-        {
-            Console.WriteLine(username);
-        }
-        */
+        
     }
 
 }
